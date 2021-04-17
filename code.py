60f673c191fcd6dfe99afa4e825850522eccd089
phanirithvij <phanirithvij2000@gmail.com>
2021-04-17 19:58:37 +0530

    fix some suff, make funcions

--- code.py
+++ code.py
@@ -5,6 +5,7 @@ import pickle
 
 # todo read from config
 URL_ROOT = "https://the-eye.eu/public/Comics/DC%20Chronology/"
+DIR_PATH = "/public/Comics/DC Chronology/"
 TELE_DEST = "https://t.me/joinchat/ZGyTW1tbXbIzYmI9"
 DELETE_LOCAL = True
 
@@ -14,7 +15,16 @@ if DELETE_LOCAL:
     _dl_str = "-d"
 
 # commands
-DL_CMD = """wget -m -np -c -U "eye02" -R "index.html*" "{}" --progress=bar:noscroll -P /root"""
+DL_CMD = [
+    "wget -m -np -c",
+    "-U", "eye02",
+    "-R", "index.html*",
+    "--progress=bar:noscroll",
+    "-P /root",
+    # "-X", "",
+    '"{0}"',
+]
+
 TELE_CMD = [
     "telegram-upload",
     "--to", f'"{TELE_DEST}"',
@@ -26,8 +36,6 @@ TELE_CMD = [
     '"{0}"',
 ]
 
-print(" ".join(TELE_CMD).format("xx"))
-
 
 def read_dir(path: Path):
     ret = {}
@@ -36,28 +44,35 @@ def read_dir(path: Path):
     for root, _, files in os.walk(path):
         for file in files:
             parent = Path(root, file).parent
-            ret[parent/file] = True
+            # skip other dirs in /root
+            if "the-eye.eu" not in parent.parts:
+                continue
+            # relative_path is the url path
+            relative_path = os.path.relpath(parent/file, storage_path)
+            if sys.platform == 'win32':
+                relative_path = relative_path.replace("\\", "/")
+            ret[relative_path] = (parent/file).stat().st_size
     return ret
 
 
 def tele_command(path: Path):
-    print()
+    print(" ".join(TELE_CMD).format(path))
 
 
-def read_dict():
+def read_db():
     if not db_path.exists():
         return {}
     with open(db_path, 'rb') as db:
         return pickle.load(db)
 
 
-def dump_dict(dictx):
+def save_db(dictx):
     with open(db_path, 'wb+') as db:
         pickle.dump(dictx, db)
 
 
 def setup():
-    storage = "/root/the-eye.eu/"
+    storage = "/root"
     db_dest = "~/data.pkl"
     if len(sys.argv) > 1:
         storage = sys.argv[1]
@@ -71,8 +86,12 @@ def setup():
 if __name__ == '__main__':
     storage_path, db_path = setup()
     print(storage_path, db_path)
-    exist_files = read_dict()
+
+    exist_files = read_db()
+    # print(exist_files)
     print(len(exist_files.keys()), "files exist in db")
+
     done_files = read_dir(storage_path)
     print(len(done_files.keys()), "files done")
-    dump_dict(done_files)
+
+    save_db(done_files)
