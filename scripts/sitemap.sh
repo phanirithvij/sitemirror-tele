bc86dc6677fb1389ce3cb478e0bd500af5dc1f16
phanirithvij <phanirithvij2000@gmail.com>
2021-04-17 20:12:12 +0530

    tee?

--- scripts/sitemap.sh
+++ scripts/sitemap.sh
@@ -1,6 +1,7 @@
 # https://stackoverflow.com/a/6747761/8608146
+# https://unix.stackexchange.com/a/128476/312058
 
 wget --spider --recursive --no-verbose -U "eye02" -R "index.html*" -m -np \
- --output-file=wgetlog.txt "https://the-eye.eu/public/Comics/DC%20Chronology/"
+ "https://the-eye.eu/public/Comics/DC%20Chronology/" 2>&1 | tee -a wgetlog.txt
 
 sed -n "s@.\+ URL:\([^ ]\+\) .\+@\1@p" wgetlog.txt | sed "s@&@\&amp;@" > sedlog.txt
